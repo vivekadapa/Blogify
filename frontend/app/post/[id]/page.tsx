d7863e0edc0dc1cfe0e5b51d6b825a40cdf1ad7f
vivekadapa <vivekchaitanya1324@gmail.com>
2024-11-22 18:00:29 +0530

    props build fix

--- frontend/app/post/[id]/page.tsx
+++ frontend/app/post/[id]/page.tsx
@@ -1,42 +1,37 @@
-
-
 import React from 'react';
 import { format } from 'date-fns';
 import styles from '@/styles/BlogPost.module.css';
 import Layout from '@/components/Layout';
-
 
 interface BlogPost {
     _id: string;
     title: string;
     content: string;
     author: {
-        email: string
-    }
+        email: string;
+    };
     createdAt: string;
 }
 
+export default async function Page({ params }: { params: { id: string }; }) {
 
-export default async function Page({
-    params,
-}: {
-    params: { id: string };
-}) {
-    const id = params.id;
+    const { id } = params;
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/posts/post/${id}`, {
         method: 'GET',
         credentials: 'include',
     });
+
     if (!res.ok) {
         return <p>Error: Post not found!</p>;
     }
 
     const post: BlogPost = await res.json();
+
     return (
         <Layout>
             <div className={`${styles.post} ${styles.fullWidthPost}`}>
-                <p className={styles.meta}>{format(new Date(post.createdAt), "MMM d, yyyy")}</p>
+                <p className={styles.meta}>{format(new Date(post.createdAt), 'MMM d, yyyy')}</p>
                 <h2 className={styles.title}>{post.title.toUpperCase()}</h2>
                 <div className={styles.content}>{post.content}</div>
                 <p className={styles.author}>By {post.author.email}</p>
